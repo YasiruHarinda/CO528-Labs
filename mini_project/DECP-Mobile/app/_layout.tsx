43e50a2333e8f5417467371fcf91da98e352bd6c
Yasiru Edirimanna <e20089@eng.pdn.ac.lk>
2026-03-13 14:53:30 +0530

    .

--- mini_project/DECP-Mobile/app/_layout.tsx
+++ mini_project/DECP-Mobile/app/_layout.tsx
@@ -1,16 +1,5 @@
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return (
-    <>
-      <StatusBar style="light" backgroundColor="#0a1628"/>
-      <Stack screenOptions={{
-        headerStyle: { backgroundColor: '#0a1628' },
-        headerTintColor: '#00d4ff',
-        headerTitleStyle: { fontWeight: 'bold' },
-        contentStyle: { backgroundColor: '#0a1628' }
-      }}/>
-    </>
-  );
+  return <Stack />;
 }
